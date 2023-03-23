062085baab73cd7b1b016c522959fe623f13ac9c
kelvinrandu <randukelvin@gmail.com>
2023-03-23 20:51:36 +0300

    added minor ui changes

--- kulaApp/components/AnimatedHeader.js
+++ kulaApp/components/AnimatedHeader.js
@@ -1,30 +1,106 @@
 import React from "react";
-import { Animated, View } from "react-native";
+import { Animated, View,Text,Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HEADER_HEIGHT = 200;
 
-const AnimatedHeader = ({ animatedValue }) => {
+const AnimatedHeader = (props) => {
+      const { name, image, price, reviews, rating, categories } =
+        props.route.params;
+    const {
+
+            animatedValue,
+          } = props;
+      const formattedCategories = categories
+        .map((cat) => cat.title)
+        .join(" • ");
+      const description = `${formattedCategories} ${
+        price ? " • " + price : ""
+      }   • 🎫 • ${rating} ⭐  (${reviews}+)`;
   const insets = useSafeAreaInsets();
     const headerHeight = animatedValue.interpolate({
       inputRange: [0, HEADER_HEIGHT + insets.top],
-      outputRange: [HEADER_HEIGHT + insets.top, insets.top + 44],
+      outputRange: [HEADER_HEIGHT + insets.top, insets.top + 100],
       extrapolate: "clamp",
     });
 
   return (
-    <Animated.View
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 10,
-        height: headerHeight,
-        backgroundColor: "lightblue",
-      }}
-    />
+    <>
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 10,
+          height: headerHeight,
+          backgroundColor: "lightblue",
+        }}
+      >
+        <RestaurantImage image={image} />
+        <RestaurantTitle name={name} />
+        <RestaurantDescription description={description} />
+      </Animated.View>
+    </>
   );
 };
+
+const RestaurantImage = (props) => (
+  <Image
+    source={{
+      uri: props.image,
+    }}
+    // style={{ width: "100%", height: 180 }}
+    style={{ width: "100%", height: "100%" }}
+  />
+);
+const RestaurantTitle = (props) => (
+  <Text
+    style={{
+      fontSize: 29,
+      zIndex: 10,
+      fontWeight: "600",
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.name}
+  </Text>
+);
+const RestaurantDescription = (props) => (
+  <>
+    <Text
+      style={{
+        marginTop: 10,
+        marginHorizontal: 15,
+        fontWeight: "700",
+        fontSize: 15.5,
+      }}
+    >
+      4.4 -vegan
+    </Text>
+    <Text
+      style={{
+        color: "gray",
+
+        marginHorizontal: 15,
+        fontWeight: "400",
+        fontSize: 15.5,
+      }}
+    >
+      Open till 8.pm
+    </Text>
+    <Text
+      style={{
+        color: "gray",
+        marginHorizontal: 15,
+        fontWeight: "400",
+        fontSize: 15.5,
+      }}
+    >
+      Click to get more information about the restaurant
+    </Text>
+  </>
+);
 
 export default AnimatedHeader;
--- kulaApp/screens/RestaurantDetails.js
+++ kulaApp/screens/RestaurantDetails.js
@@ -8,9 +8,73 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SafeAreaView from "react-native-safe-area-view";
 import AnimatedHeader from "../components/AnimatedHeader";
 
+const DATA = [
+  {
+    id: 1,
+    title: "The Hunger Games",
+  },
+  {
+    id: 2,
+    title: "Harry Potter and the Order of the Phoenix",
+  },
+  {
+    id: 3,
+    title: "To Kill a Mockingbird",
+  },
+  {
+    id: 4,
+    title: "Pride and Prejudice",
+  },
+  {
+    id: 5,
+    title: "Twilight",
+  },
+  {
+    id: 6,
+    title: "The Book Thief",
+  },
+  {
+    id: 7,
+    title: "The Chronicles of Narnia",
+  },
+  {
+    id: 8,
+    title: "Animal Farm",
+  },
+  {
+    id: 9,
+    title: "Gone with the Wind",
+  },
+  {
+    id: 10,
+    title: "The Shadow of the Wind",
+  },
+  {
+    id: 11,
+    title: "The Fault in Our Stars",
+  },
+  {
+    id: 12,
+    title: "The Hitchhiker's Guide to the Galaxy",
+  },
+  {
+    id: 13,
+    title: "The Giving Tree",
+  },
+  {
+    id: 14,
+    title: "Wuthering Heights",
+  },
+  {
+    id: 15,
+    title: "The Da Vinci Code",
+  },
+];
+
 const H_MAX_HEIGHT = 150;
 const H_MIN_HEIGHT = 52;
 const H_SCROLL_DISTANCE = H_MAX_HEIGHT - H_MIN_HEIGHT;
+
 
 export default function RestaurantDetails({ route ,navigation}) {
    const offset = useRef(new Animated.Value(0)).current;
@@ -33,10 +97,48 @@ export default function RestaurantDetails({ route ,navigation}) {
       // </View>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always" }}>
-          <View style={{ flex: 1, alignItems: "center" }}>
-            <AnimatedHeader animatedValue={offset} />
-            <Text>Open up App.js to start working on your app!</Text>
-          </View>
+          <AnimatedHeader route={route} animatedValue={offset} />
+
+          <Divider width={1.8} style={{ marginVertical: 20 }} />
+
+          {/* <ScrollView
+            style={{ flex: 1, backgroundColor: "white" }}
+            contentContainerStyle={{
+              alignItems: "center",
+              paddingTop: 220,
+              paddingHorizontal: 20,
+            }}
+            showsVerticalScrollIndicator={false}
+            scrollEventThrottle={16}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { y: offset } } }],
+              { useNativeDriver: false }
+            )}
+          >
+            <MenuItems restaurantName={route.params.name} />
+          </ScrollView> */}
+          <ScrollView
+            style={{ flex: 1, backgroundColor: "white" }}
+            contentContainerStyle={{
+              // alignItems: "center",
+              alignItems: "flex-start",
+              paddingTop: 300,
+              zIndex: 1,
+              paddingHorizontal: 20,
+            }}
+            showsVerticalScrollIndicator={false}
+            scrollEventThrottle={16}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { y: offset } } }],
+              { useNativeDriver: false }
+            )}
+          >
+            <ViewCart
+              navigation={navigation}
+              restaurantName={route.params.name}
+            />
+            <MenuItems restaurantName={route.params.name} />
+          </ScrollView>
         </SafeAreaView>
       </SafeAreaProvider>
     );
